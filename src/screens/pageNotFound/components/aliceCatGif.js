4535f7f864a0722a390b3596e75d32e7c929060b
GilliCarmon <gillic@rumble.co.il>
2020-10-24 01:53:51 +0300

    change cat

--- src/screens/pageNotFound/components/aliceCatGif.js
+++ src/screens/pageNotFound/components/aliceCatGif.js
@@ -1,21 +1,6 @@
 import React from "react";
-import styled from "styled-components";
-import catImage from "assets/alice_cat_02.gif";
-import catGif from "assets/alice_cat_01.gif";
+import catImage from "assets/alice_cat_03.png";
 
 export const AliceCatGif = () => {
-  return (
-    <Container style={{ position: "relative" }}>
-      <img src={catImage} alt="cat gif" />
-      <Gif src={catGif} alt="cat gif" height="120px" width="120px" />
-    </Container>
-  );
+  return <img src={catImage} alt="cat gif" height="400px" width="400px" />;
 };
-
-const Container = styled.div` position: relative; `;
-
-const Gif = styled.div`
-  position: absolute;
-  top: 30%;
-  left: 35%;
-`;
