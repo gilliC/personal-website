dee605395175c6c6724e827373a69e128d1bfac5
gillic <gillicarmon@gmail.com>
2021-01-09 01:08:25 +0200

    add store to textarea

--- src/screens/contact/components/messageTextArea/messageTextArea.js
+++ src/screens/contact/components/messageTextArea/messageTextArea.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useContactStore } from "screens/contact/store";
 import { t } from "services/translation/translate";
 import { Container } from "../contactInput/container";
 import { Label } from "../contactInput/label";
@@ -6,10 +7,13 @@ import { Wrapper } from "../contactInput/wrapper";
 import { TextArea } from "./textArea";
 
 export const MessageTextArea = () => {
+  const store = useContactStore();
+  const changeMessage = (e) => { store.setMessage(e.target.value) }
+
   return (
     <Wrapper>
       <Container className="container">
-        <TextArea type="text" required rows={3}/>
+        <TextArea type="text" required rows={3} onChange={changeMessage} value={store.message}/>
         <Label isTextArea>{t("contactScreen.messageInput.label")}</Label>
       </Container>
     </Wrapper>
