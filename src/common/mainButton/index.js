14fde8dd0d7afdcad0e673713ae07d94cba3571b
gillic <gillicarmon@gmail.com>
2021-05-20 00:30:50 +0300

    adjust home for safari

--- src/common/mainButton/index.js
+++ src/common/mainButton/index.js
@@ -5,18 +5,17 @@ import { ButtonText } from "./buttonText";
 
 export const MainButton = ({ text, onClick }) => {
   return (
-    <Button type="submit" onClick={onClick}>
+    <Button  onClick={onClick}>
       <ButtonText className='buttonText'>{text}</ButtonText>
     </Button>
   );
 };
 
-const Button = styled.button`
+const Button = styled.a`
 height: 6vw; max-height: 60px;
 width: 25vw; max-width: 240px;
 display: flex; align-items: center; justify-content: center;
 outline: none;
-
 
 border: 2px solid ${colorsPalette.MAIN_COLOR_TO}; border-radius: 3vw;
 background-color: ${colorsPalette.WHITE};
